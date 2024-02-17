c1aa809deba0038cc9f7bba99b0c1a8616a9daa9
tte4enka <geravanya963@gmail.com>
2024-02-17 22:39:21 +0300

    8c

--- 17lab/Exp2/Program.cs
+++ 17lab/Exp2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 /*
  Задание 1. Написать программу, в которой создаются объекты различных классов и
 помещаются в массив, после чего массив просматривается.
@@ -107,27 +107,98 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            ArrayList list = new ArrayList();
-            Place[] places = new Place[4];
-            Region gomelsk = new Region("Африка", "Гомельская");
-            Region pinsk = new Region("Северная Америка", "Пинская");
-            City gomel = new City("Евразия", "Гомель", 200000000);
-            Metropolis moskva = new Metropolis("Южная Америка", "Москва");
-            places[0] = gomelsk; places[1] = pinsk; places[2] = gomel; places[3] = moskva;
-            list.AddRange(places);
-            Console.WriteLine("До сортировки:");
-            foreach (Place obj in list)
-            {
-                obj.GetInfo();
-            }
-            list.Sort();
-            Console.WriteLine("\n\n\nПосле сортировки:");
-            foreach (Place obj in list)
-            {
-                obj.GetInfo();
-            }
+            Queue<Place> places = new Queue<Place>();
 
-            Console.ReadKey();
+            bool isActive = true;
+            while (isActive)
+            {
+                Console.WriteLine("---Menu---");
+                Console.WriteLine("1 - заполнить очередь базовыми значениями.");
+                Console.WriteLine("2 - добавить элемент.");
+                Console.WriteLine("3 - извлечь и вывести первый элемент.");
+                Console.WriteLine("4 - просмотреть первый элемент");
+                Console.WriteLine("5 - выход");
+                try
+                {
+                    switch (int.Parse(Console.ReadLine()))
+                    {
+                        case 1:
+                            Region pinsk = new Region("Северная Америка", "Пинская");
+                            City gomel = new City("Евразия", "Гомель", 200000000);
+                            Metropolis moskva = new Metropolis("Южная Америка", "Москва");
+                            places.Enqueue(pinsk);
+                            places.Enqueue(gomel);
+                            places.Enqueue(moskva);
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("---Add Menu---\n" +
+                                "1 - регион\n" + "2 - город\n" + "3 - мегаполис");
+                            switch (int.Parse(Console.ReadLine()))
+                            {
+                                case 1:
+                                    Console.Write("Введите континент:");
+                                    string cont = Console.ReadLine();
+                                    Console.Write("Введите название региона:");
+                                    string name = Console.ReadLine();
+                                    places.Enqueue(new Region(cont, name));
+                                    Console.Clear();
+                                    Console.WriteLine("Регион успешно добавлен!");
+                                    break;
+                                case 2:
+                                    Console.Write("Введите континент:");
+                                    cont = Console.ReadLine();
+                                    Console.Write("Введите название города:");
+                                    name = Console.ReadLine();
+                                    Console.Write("Введите кол-во людей живущих в городе:");
+                                    long count = long.Parse(Console.ReadLine());
+                                    places.Enqueue(new City(cont, name, count));
+                                    Console.Clear();
+                                    Console.WriteLine("Город успешно добавлен!");
+                                    break;
+                                case 3:
+                                    Console.Write("Введите континент:");
+                                    cont = Console.ReadLine();
+                                    Console.Write("Введите название мегаполиса:");
+                                    name = Console.ReadLine();
+                                    places.Enqueue(new Metropolis(cont, name));
+                                    Console.Clear();
+                                    Console.WriteLine("Мегаполис успешно добавлен!");
+                                    break;
+                            }
+                            break;
+                        case 3:
+                            if(places.Count != 0)
+                            {
+                                Place a = places.Dequeue();
+                                a.GetInfo();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Очередь пуста!");
+                            }
+                            break;
+                        case 4:
+                            if (places.Count != 0)
+                            {
+                                Place a = places.Peek();
+                                a.GetInfo();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Очередь пуста!");
+                            }
+                            break;
+                        case 5:
+                            isActive = false;
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели что-то неверно!");
+                }
+            }
         }
     }
 }
