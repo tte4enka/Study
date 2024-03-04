38e3290fa748cf17d9fed898acd9ff6919ac4789
Life <geravanya963@gmail.com>
2024-03-04 15:07:06 +0300

    1 task complete

---
+++ 1task/Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1task
+{
+    public class Calculate : IArithmetic
+    {
+        public double add(double x, double y)
+        {
+            return x + y;
+        }
+        public double subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public double multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double divide(double x, double y)
+        {
+            return x / y;
+        }
+        public double sqrt(double x)
+        {
+            return Math.Sqrt(x);
+        }
+        public double abs(double x)
+        {
+            return Math.Abs(x);
+        }
+        public double pow(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+    }
+}
