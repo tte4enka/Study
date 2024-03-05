ec584fe229d2ec9b11c845a5e99ea157eaf2652c
Life <geravanya963@gmail.com>
2024-03-05 10:05:19 +0300

    write main

--- PR10/Program.cs
+++ PR10/Program.cs
@@ -10,7 +10,21 @@ namespace _1task
     {
         static void Main(string[] args)
         {
+            Region region = new Region("Африка", "Гомельская область");
+            region.ChangePlace();
+            region.ChangeName("Гомельская");
+            region.GetPlace();
 
+            City city = new City("Африка", "Гомель", 1888888);
+            city.GetPlace();
+            city.SetNameOfCity("Минск");
+            city.ChangePlace();
+            city.SetCountPeople(10);
+
+            Metropolis metropolis = new Metropolis("Африка", "Москва");
+            metropolis.ChangePlace();
+            metropolis.ChangeName("НеМосква");
+            metropolis.GetPlace();
         }
     }
 }
