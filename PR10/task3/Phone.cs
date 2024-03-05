9df83f75199b6b8b4548d4faf37cf15034495a86
Life <geravanya963@gmail.com>
2024-03-05 11:18:56 +0300

    Add class phone

---
+++ PR10/task3/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3task
+{
+    public abstract class Phone
+    {
+        private string brand;
+        private int functionCount;
+
+        public Phone() 
+        {
+            brand = "None";
+            functionCount = 1;
+        }
+        public string _brand { get; set; }
+        public int _functionCount {  get; set; }
+
+        public double Cost(int k)
+        {
+            return 40 * Math.Log(k);
+        }
+    }
+}
