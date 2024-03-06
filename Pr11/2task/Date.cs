27f99d177353be87b7ed55246800b60f4095938f
Life <geravanya963@gmail.com>
2024-03-06 16:43:59 +0300

    copy second task from 10Pr

---
+++ Pr11/2task/Date.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2task
+{
+    public class Date : Triad
+    {
+
+        public Date(int day, int month, int year) : base(day, month, year)
+        {
+            First = year;
+            Second = month;
+            Third = day;
+        }
+
+        public static bool operator ==(Date a, Date b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Date a, Date b)
+        {
+            return !(a == b);
+        }
+        public static bool operator >(Date a, Date b)
+        {
+            DateTime first = new DateTime(a.First, a.Second, a.Third);
+            DateTime second = new DateTime(b.First, b.Second, b.Third);
+
+            return first > second;
+        }
+
+        public static bool operator <(Date a, Date b)
+        {
+            DateTime first = new DateTime(a.First, a.Second, a.Third);
+            DateTime second = new DateTime(b.First, b.Second, b.Third);
+
+            return first < second;
+        }
+
+        public static bool operator >=(Date a, Date b)
+        {
+            DateTime first = new DateTime(a.First, a.Second, a.Third);
+            DateTime second = new DateTime(b.First, b.Second, b.Third);
+
+            return first >= second;
+        }
+
+        public static bool operator <=(Date a, Date b)
+        {
+            DateTime first = new DateTime(a.First, a.Second, a.Third);
+            DateTime second = new DateTime(b.First, b.Second, b.Third);
+
+            return first <= second;
+        }
+    }
+}
---
+++ Pr11/2task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date1 = new Date(1, 3, 2026);
+            Date date2 = new Date(1, 3, 2024);
+
+            Console.WriteLine(date2 == date1);
+            Console.WriteLine(date1 > date2);
+
+            Console.ReadLine();
+        }
+    }
+}
