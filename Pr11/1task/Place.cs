307cd140ad10131dffcd4cfdd4ea8de7b3431eea
Life <geravanya963@gmail.com>
2024-03-06 16:36:57 +0300

    Взял основу из прошлой практички

---
+++ Pr11/1task/Place.cs
@@ -0,0 +1,29 @@
+﻿namespace _1task
+{
+    public abstract class Place
+    {
+        private string Continent;
+        public string continent { get { return Continent; } set { } }
+
+        public Place(string continent)
+        {
+            Continent = continent;
+        }
+
+        public Place()
+        {
+            Continent = "Африка";
+        }
+        public abstract void GetPlace();
+        public abstract void ChangePlace();
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Place temp = (Place)obj;
+            return Continent == temp.Continent;
+        }
+    }
+
+}
