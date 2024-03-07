06c63a504836fdbf789ea21cd2cd5f73043dda38
Life <geravanya963@gmail.com>
2024-03-07 09:25:01 +0300

    1 task ready

--- Pr12/Program.cs
+++ Pr12/Program.cs
@@ -4,22 +4,29 @@ namespace _1task
 {
     internal class Program
     {
+        delegate void GetAll();
+        delegate void ChangeAll();
         static void Main(string[] args)
-        {         
+        {
             Region region = new Region("Африка", "Гомельская область");
-            region.ChangePlace();
-            region.ChangeName("Гомельская");
-            region.GetPlace();
             City city = new City("Африка", "Гомель", 1888888);
-            city.GetPlace();
-            city.SetNameOfCity("Минск");
-            city.ChangePlace();
-            city.SetCountPeople(10);
-
             Metropolis metropolis = new Metropolis("Африка", "Москва");
-            metropolis.ChangePlace();
-            metropolis.ChangeName("НеМосква");
-            metropolis.GetPlace();
+
+            GetAll get = () =>
+            {
+                region.GetPlace();
+                city.GetPlace();
+                metropolis.GetPlace();
+            };
+            get.Invoke();
+
+            ChangeAll change = () =>
+            {
+                region.ChangePlace();
+                city.ChangePlace();
+                metropolis.ChangePlace();
+            };
+            get.Invoke();
 
             Console.ReadKey();
         }
