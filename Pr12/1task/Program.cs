40ace08c56c181960f95780b78603a5bfbb04a6d
Life <geravanya963@gmail.com>
2024-03-07 09:29:20 +0300

    foldered 1 task

---
+++ Pr12/1task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1task
+{
+    internal class Program
+    {
+        delegate void GetAll();
+        delegate void ChangeAll();
+        static void Main(string[] args)
+        {
+            Region region = new Region("Африка", "Гомельская область");
+            City city = new City("Африка", "Гомель", 1888888);
+            Metropolis metropolis = new Metropolis("Африка", "Москва");
+
+            GetAll get = () =>
+            {
+                region.GetPlace();
+                city.GetPlace();
+                metropolis.GetPlace();
+            };
+            get.Invoke();
+
+            ChangeAll change = () =>
+            {
+                region.ChangePlace();
+                city.ChangePlace();
+                metropolis.ChangePlace();
+            };
+            get.Invoke();
+
+            Console.ReadKey();
+        }
+    }
+}
