f625277eaf189e13ac0601c24b51ad8c3511e5f4
Life <geravanya963@gmail.com>
2024-03-07 09:50:20 +0300

    add Assistant class

---
+++ Pr12/3task/Assistant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Linq;
+/*
+ Класс продавец «Assistant» - наследник класса «Person», содержит все
+свойства родительского класса, а также и дополнительные свойства Стаж
+(Seniority)  и Зарплата (Salarysum).  Зарплата рассчитывается как  оклад плюс
+стаж умноженный на 2% (Salarysum =Salary+ Seniority*0.2);
+*/
+namespace _3task
+{
+    public class Assistant : Person
+    {
+        private double seniority;
+        private double salarysum;
+
+        public Assistant(): base()
+        {
+            seniority = 0;
+            salarysum = 0;
+        }
+
+        public Assistant(string name, string fio, long salary, long seniority, long salarysum): base(name, fio, salary)
+        {
+            this.seniority = seniority;
+            this.salarysum = salarysum;
+            CalculateSalary();
+        }
+        public double _seniority { get { return seniority; } }
+        public double _salarysum { get { return salarysum; } }
+
+        public void CalculateSalary()
+        {
+            this.salarysum = _salary + seniority * 0.2;
+        }
+    }
+}
