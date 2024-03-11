0693f74ce1ec68919e696f00be32e27cfa90b165
Life <geravanya963@gmail.com>
2024-03-11 21:55:41 +0300

    Pr complete

---
+++ Pr13/5task/TemperatureEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace _5task
+{
+    public class TemperatureEventArgs : EventArgs
+    {
+        public int Value { get; set; }
+    }
+}
---
+++ Pr13/5task/Thermometer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _5task
+{
+    public class Thermometer : IEventTrigger
+    {
+        private int maxTemperature;
+        public Thermometer(int maxTemperature)
+        {
+            this.maxTemperature = maxTemperature;
+        }
+        public event EventHandler<TemperatureEventArgs> OnTemperatureChanged = delegate { };
+        public int Temperature
+        {
+            set
+            {
+                var temperature = value;
+                if (temperature > maxTemperature)
+                {
+                    var temperatureValue = new TemperatureEventArgs
+                    {
+                        Value = temperature,
+                    };
+                    OnTemperatureChanged(this, temperatureValue);
+                }
+            }
+        }
+    }
+}
