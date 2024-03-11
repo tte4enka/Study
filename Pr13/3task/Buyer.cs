ae00665f99213767b50181c9474cf42ec74e5c37
Life <geravanya963@gmail.com>
2024-03-11 12:15:54 +0300

    init

---
+++ Pr13/3task/Buyer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3task
+{
+    public class Buyer
+    {
+        private double _money;
+        private double[] _cart;
+
+        public Buyer()
+        {
+        }
+
+        public Buyer(double money)
+        {
+            Money = money;
+        }
+
+        public double Money { get => _money; set => _money = value; }
+        public double[] Cart { get => _cart; set => _cart = value; }
+    }
+}
---
+++ Pr13/3task/SuperMarket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace _3task
+{
+    public class SuperMarket
+    {
+        private Queue _queue;
+
+        public SuperMarket()
+        {
+        }
+
+        public SuperMarket(Queue queue)
+        {
+            Queue = queue;
+        }
+
+        public Queue Queue { get => _queue; set => _queue = value; }
+    }
+}
