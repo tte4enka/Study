3650a26bed246c182940d6c26bbc6c79260fb6b9
Life <geravanya963@gmail.com>
2024-03-12 08:45:23 +0300

    1 task complete

---
+++ Pr14/1task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+/*
+ Создайте многопоточное приложение. В одном потоке выводите первую букву вашего имени, во втором потоке выводите первую букву вашей фамилии.
+ */
+namespace _1task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread first = new Thread(() => { Console.WriteLine("И"); });
+            Thread second = new Thread(() => { Console.WriteLine("Г"); });
+
+            first.Start();
+            second.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
